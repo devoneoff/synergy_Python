33df4ccee41b313d65e72c2bd0aa1a0b2903bffb
DevoneOff <111913427+devoneoff@users.noreply.github.com>
2025-03-25 17:57:31 +0300

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,15 @@
+# Задача 1
+type_animal = input("Вид питомца: ")
+age_animal = input("Возраст питомца: ")
+name_animal = input("Кличка питомца: ")
+print("Это", type_animal, "по кличке", name_animal, ". Возраст:", age_animal, "года")
+
+# Задача 2
+stage1 = input("Первая стадия: ") # Australopithecus 
+stage2 = input("Вторая стадия: ") # Homo habilis
+stage3 = input("Третья стадия: ") #  Homo erectus
+stage4 = input("Четвёртая стадия: ") # Homo neanderthalensis
+stage5 = input("Пятая стадия: ") # Homo sapiens
+stage6 = input("Шестая стадия: ") # Homo sapiens sapiens
+print (stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ") 
+# Australopithecus => Homo habilis => Homo erectus => Homo neanderthal => Homo sapiens => Homo sapiens sapiens
